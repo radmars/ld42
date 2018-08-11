2b0c459deca0ea9cb85b25281ec8ff02690bf34a
Eugene Marcotte <emarcotte@gmail.com>
2018-08-10 21:52:52 -0400

    camera swivel and worst chairman fisix i can muster

---
+++ Assets/scripts/Chairman.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chairman : MonoBehaviour {
+	private Rigidbody body;
+
+	// Use this for initialization
+	void Start () {
+		body = GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKeyDown)
+		{
+			body.AddForceAtPosition(new Vector3(1f, .1f, 0), Vector3.forward, ForceMode.Impulse);
+		}
+	}
+}

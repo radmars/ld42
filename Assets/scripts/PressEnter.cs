2d8250214c7d06a845d4e79293cbf3146f93ea26
Eugene Marcotte <emarcotte@gmail.com>
2018-08-11 23:14:56 -0400

    splash screen and muzak

---
+++ Assets/scripts/PressEnter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PressEnter : MonoBehaviour {
+
+	void Start () {
+		StartCoroutine(Blink());
+	}
+
+	private IEnumerator Blink()
+	{
+		while(true)
+		{
+			yield return new WaitForSeconds(.5f);
+			GetComponent<Text>().enabled = !GetComponent<Text>().enabled;
+		}
+	}
+
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Return)) {
+			UnityEngine.SceneManagement.SceneManager.LoadScene("em room");
+		}
+	}
+}

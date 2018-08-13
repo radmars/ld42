105986922f1b92ce3228251d199583f451f40be5
Eugene Marcotte <emarcotte@gmail.com>
2018-08-12 21:45:59 -0400

    main scene tweakin

--- Assets/scripts/Elevator.cs
+++ Assets/scripts/Elevator.cs
@@ -37,17 +37,17 @@ public class Elevator : MonoBehaviour {
 		float startTime = Time.time;
 		float openTime = startTime + openDuration;
 
-		Vector3 leftStart = leftDoor.transform.position;
+		Vector3 leftStart = leftDoor.transform.localPosition;
 		Vector3 leftTarget = leftStart + new Vector3(0, 0, -offset);
 
-		Vector3 rightStart = rightDoor.transform.position;
+		Vector3 rightStart = rightDoor.transform.localPosition;
 		Vector3 rightTarget = rightStart + new Vector3(0, 0, offset);
 
 		while(Time.time < openTime)
 		{
 			var progress = (Time.time - startTime) / openDuration;
-			leftDoor.transform.position = Vector3.Lerp(leftStart, leftTarget, progress);
-			rightDoor.transform.position = Vector3.Lerp(rightStart, rightTarget, progress);
+			leftDoor.transform.localPosition = Vector3.Lerp(leftStart, leftTarget, progress);
+			rightDoor.transform.localPosition = Vector3.Lerp(rightStart, rightTarget, progress);
 			yield return new WaitForFixedUpdate();
 		}
 	}
